4f58a7c59dbb1784c3c250db0262d4123fc2fb82
Iain Taylor <iain.taylor.94@gmail.com>
2023-06-28 14:41:53 +0000

    completed program

--- .ccls-cache/@home@runner@Ch5Q2/main.c
+++ .ccls-cache/@home@runner@Ch5Q2/main.c
@@ -1,6 +1,19 @@
+// Program to create a table of n vs n^2
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  // Table heading
+  printf ("TABLE FOR N^2\n");
+  printf (" n     n^2 \n");
+  printf ("--- ---------\n");
+
+  for (int i = 1; i <= 10; i++) {
+    printf ("%3i      %4i   \n", i, i*i);
+  }
+
+  printf ("\n");
+  
   return 0;
 }
